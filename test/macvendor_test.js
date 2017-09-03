a261a8f58d57a3d8f43751239dc8c375c9460d97
Rimpei Kunimoto <ht14a022@oecu.jp>
2017-09-04 02:03:05 +0900

    Fix Test

--- test/macvendor_test.js
+++ test/macvendor_test.js
@@ -4,27 +4,37 @@ var assert = require('assert');
 
 vows.describe('macvendor').addBatch({
   'An empty mac': {
-    topic: function() {
+    topic: function () {
       macvendor('', this.callback);
     },
-    'should be undefined': function(err, vendor) {
-      assert.isUndefined(vendor);
+    'should be undefined': function (err, vendor) {
+      assert.strictEqual(vendor, 'No MAC address provided\n');
     }
   },
   'A non existent mac': {
-    topic: function() {
+    topic: function () {
       macvendor('01:23:45:67:89:ab', this.callback);
     },
-    'should be undefined': function(err, vendor) {
-      assert.isUndefined(vendor);
+    'should be undefined': function (err, vendor) {
+      assert.strictEqual(vendor, 'Vendor not found\n');
     }
   },
   'An apple mac': {
-    topic: function() {
+    topic: function () {
       macvendor('00:1C:B3:09:85:15', this.callback);
     },
-    'should be undefined': function(err, vendor) {
-      assert.strictEqual(vendor, 'APPLE, INC');
+    'should be undefined': function (err, vendor) {
+      assert.strictEqual(vendor, 'Apple, Inc.');
+    }
+  },
+  'An apple mac with promise': {
+    topic: function () {
+      macvendor('00:1C:B3:09:85:15')
+        .then(vendor => this.callback(false, vendor))
+        .catch(err => this.callback(err));
+    },
+    'should be undefined': function (err, vendor) {
+      assert.strictEqual(vendor, 'Apple, Inc.');
     }
   }
 }).export(module);
